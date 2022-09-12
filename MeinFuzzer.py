f9ca42b5c8bc9e527006f2dfc05f684e23ffdb19
Daniel Serbu <daniserbu@outlook.com>
2022-09-12 06:01:09 +0300

    Scan for paths recursively

--- MeinFuzzer.py
+++ MeinFuzzer.py
@@ -25,68 +25,62 @@ if os.stat(fuzzlist).st_size == 0:
 
 fuzzFile = open(fuzzlist).read()
 paths = fuzzFile.splitlines()
-discovered_paths = []
+all_discovered_paths = []
 paths_already_checked = []
-fullUrl = ""
+
 
 ### Utils ###
-def check_for_path(paths, url):
-    discovered_at_least_one_path = True
+def check_for_paths_in_url(paths, url):
+    discovered_paths = []
     for path in paths:
         fullUrl = url.replace("FUZZ", path)
         # Do not scan again in paths that do not exist.
         if fullUrl not in paths_already_checked:
-            #print("Checking URL " + fullUrl)
-            try:
-                request = requests.get(fullUrl)
-                paths_already_checked.append(fullUrl)
-            except requests.ConnectionError:
-                print("discovered at least one path set to false")
-                discovered_at_least_one_path = False
+            request = requests.get(fullUrl)
+            paths_already_checked.append(fullUrl)
+            if request.status_code != 200:
                 pass
             else:
                 if not request.status_code == 404:
                     print("[+] Discovered path:", fullUrl)
                     discovered_paths.append(fullUrl)
-    return discovered_at_least_one_path
+    # Finally, if any urls found inside this function, append the list to all_discovered_paths
+    if len(discovered_paths) != 0:
+        all_discovered_paths.extend(discovered_paths)
+    return discovered_paths
+
 
 # Directory Enumeration related only!
-def check_for_path_in_urls(paths, urls):
-    discovered_at_least_one_path = True
-    # split discovered_paths list in 2 (assuming everyone nowadays has at least 2 threats at hand :D)
-    #half_length = len(discovered_paths) // 2
-    #first_half, second_half = discovered_paths[:half_length], discovered_paths[half_length:]
-    #print("First half is " + str(first_half))
-    #print("Second half is " + str(second_half))
+def check_for_paths_in_urls(paths, urls):
+    discovered_paths = []
     for url in urls:
         if url not in paths_already_checked:
-            discovered_at_least_one_path = check_for_path(paths, url)
-    return discovered_at_least_one_path
+            discovered_paths.extend(check_for_paths_in_url(paths, url))
+    return discovered_paths
+
+
 ### Utils ###
 
-check_for_path(paths, initialUrl)
-print("Discovered paths is ")
-print(discovered_paths)
+check_for_paths_in_url(paths, initialUrl)
 
-if direnum and len(discovered_paths) != 0:
+if direnum and len(all_discovered_paths) != 0:
     answer = input("Do you want to continue checking inside previously found paths? [Y/n]")
     continueCheckingForPaths = True
     if any(answer.lower() == a for a in ["no", 'n', "NO", 'N', '0']):
         continueCheckingForPaths = False
+
     if continueCheckingForPaths:
-        discovered_at_least_one_path = True
-        while discovered_at_least_one_path:
-            # Add fuzz to end of each element in previously discovered paths
-            url = ["{}/{}".format(i, "FUZZ") for i in discovered_paths]
-            discovered_at_least_one_path = check_for_path_in_urls(paths, url)
-            if not discovered_at_least_one_path:
-                print("break")
+        # Add fuzz to end of each element in previously discovered paths
+        fuzzDiscoveredPaths = ["{}/{}".format(i, "FUZZ") for i in all_discovered_paths]
+        while True:
+            discovered_paths = check_for_paths_in_urls(paths, fuzzDiscoveredPaths)
+            if len(discovered_paths) != 0:
+                fuzzDiscoveredPaths = ["{}/{}".format(i, "FUZZ") for i in discovered_paths]
+            elif len(discovered_paths) == 0:
                 break
-        print("Discovered paths is ")
-        print(discovered_paths)
 
 with open("discovered_paths.txt", "w") as file:
-    for discoveredPath in discovered_paths:
+    for discoveredPath in all_discovered_paths:
         file.write(discoveredPath + "\n")
 
 print("Finished.")
